b4fcab73aaef73691b2b2947994ea7d79c01d691
bmarolleau <benoit.marolleau@fr.ibm.com>
2020-07-22 18:07:22 +0200

    initial commit

---
+++ camel-amq-fakeapp/src/main/java/org/apache/camel/opentracing/propagation/CamelHeadersExtractAdapter.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.opentracing.propagation;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import io.opentracing.propagation.TextMap;
+
+public final class CamelHeadersExtractAdapter implements TextMap {
+	
+	// As per the JMS spec, header names must be valid Java identifier part characters.
+	// This means that any header names that contain illegal characters (- for example) should be handled correctly
+	// Opentracing java-jms does it as follows, which is not compatible with the Camel implementation.
+	static final String JMS_DASH = "_$dash$_";
+	 
+    private final Map<String, String> map = new HashMap<>();
+
+    public CamelHeadersExtractAdapter(final Map<String, Object> map) {
+        // Extract string valued map entries
+        map.entrySet().stream().filter(e -> e.getValue() instanceof String).forEach(e ->
+            this.map.put(e.getKey().replace(JMS_DASH, "-"), (String) e.getValue()));
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, String>> iterator() {
+        return map.entrySet().iterator();
+    }
+
+    @Override
+    public void put(String key, String value) {
+        throw new UnsupportedOperationException("CamelHeadersExtractAdapter should only be used with Tracer.extract()");
+    }
+}
